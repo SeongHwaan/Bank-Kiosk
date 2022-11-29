be690bbbd97e099b23b57c5fde17df46aadaff85
Wontory <tjddnjsjo@kakao.com>
2022-11-29 13:39:07 +0900

    code refactor

--- src/ui/SetupPanel.java
+++ src/ui/SetupPanel.java
@@ -14,12 +14,14 @@ public class SetupPanel extends JPanel {
         }
 
         JLabel title = new JLabel("설정");
+        title.setHorizontalAlignment(JLabel.CENTER);
         title.setFont(new Font("", Font.BOLD, 28));
 
         gbc[0].gridx = 0;
         gbc[0].gridy = 0;
         gbc[0].weightx = 1;
         gbc[0].fill = GridBagConstraints.BOTH;
+        gbc[0].ipady = 50;
         add(title, gbc[0]);
     }
 
